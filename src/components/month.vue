d9dbae96943e71889ec6b926bb9aa084307f03f4
dkilfoyle <dkilfoyle@xtra.co.nz>
2021-08-30 20:39:48 +1200

    start

---
+++ src/components/month.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>My component</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useMainStore } from '../stores/main';
+export default defineComponent({
+  // name: 'ComponentName'
+  setup() {
+    const store = useMainStore();
+    console.log(store);
+  },
+});
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="row items-center justify-evenly"> <month></month> </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import month from '../components/month.vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { month },
+  setup() {
+    return {};
+  },
+});
+</script>
