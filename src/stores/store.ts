7388d8b89a244e2d42b351df6053d8fa0e109d8d
dkilfoyle <dkilfoyle@xtra.co.nz>
2021-09-18 10:13:37 +1200

    datetime lookup

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -28,12 +28,13 @@ import {
 } from 'date-fns';
 
 import { LoadingBar } from 'quasar';
-import { store } from 'quasar/wrappers';
 LoadingBar.setDefaults({
   color: 'cyan',
   size: '2px',
   position: 'top',
 });
+
+type RosterLookup = Record<string, Array<RosterEntry>>;
 
 const isSameDay = (d1: Date, d2: Date) => {
   return (
@@ -42,6 +43,13 @@ const isSameDay = (d1: Date, d2: Date) => {
     d1.getFullYear() == d2.getFullYear()
   );
   // return d1.getTime() == d2.getTime();
+};
+
+const getEntryTimestamp = (date: Date, time: string) => {
+  return `${date.getFullYear()}${date
+    .getMonth()
+    .toString()
+    .padStart(2, '0')}${date.getDate().toString().padStart(2, '0')}${time}`;
 };
 
 export type Time = 'AM' | 'PM';
@@ -100,6 +108,14 @@ export const useStore = defineStore('main', {
               entry.date >= state.startDate && entry.date <= this.endDate
           )
         : [];
+    },
+    rosterDateTime(): RosterLookup {
+      return this.roster.reduce((lookupTable, entry) => {
+        const id = getEntryTimestamp(entry.date, entry.time);
+        if (lookupTable[id]) lookupTable[id].push(entry);
+        else lookupTable[id] = [entry];
+        return lookupTable;
+      }, <RosterLookup>{});
     },
     endDate: (state) => addDays(state.startDate, state.numWeeks * 7 - 1),
     dates(state): Array<Date> {
@@ -310,6 +326,11 @@ export const useStore = defineStore('main', {
       // .map((date) => subDays(date, 1));
     },
 
+    getRosterAtTime(date: Date, time: string) {
+      const id = getEntryTimestamp(date, time);
+      return this.rosterDateTime[id] || [];
+    },
+
     /**
      * set activity for date/time/smo, create new roster entry if needed
      */
@@ -477,11 +498,8 @@ export const useStore = defineStore('main', {
     // ActivityView utilities
 
     getAssignedSMOs(date: Date, time: Time, activityName: string) {
-      return this.roster.filter(
-        (entry) =>
-          isSameDay(entry.date, date) &&
-          entry.time == time &&
-          entry.activity == activityName
+      return this.getRosterAtTime(date, time).filter(
+        (entry) => entry.activity == activityName
       );
     },
     getAllowedSMOs(date: Date, activityName: string) {
@@ -511,12 +529,25 @@ export const useStore = defineStore('main', {
         reasons: Array<string>(),
       };
 
-      const foundDateMatches = this.roster.filter(
-        (entry) => isSameDay(entry.date, date) && entry.activity == activityName
-      );
-      const foundTimeMatches = foundDateMatches.filter(
-        (entry) => entry.time == time
-      );
+      const foundMatches = {
+        AM: this.getRosterAtTime(date, 'AM').filter(
+          (entry) => entry.activity == activityName
+        ),
+        PM: this.getRosterAtTime(date, 'PM').filter(
+          (entry) => entry.activity == activityName
+        ),
+      };
+
+      const foundTimeMatches = foundMatches[time].length;
+      const foundDateMatches =
+        foundMatches['AM'].length + foundMatches['PM'].length;
+
+      // const foundDateMatches = this.roster.filter(
+      //   (entry) => isSameDay(entry.date, date) && entry.activity == activityName
+      // );
+      // const foundTimeMatches = foundDateMatches.filter(
+      //   (entry) => entry.time == time
+      // );
 
       const isAllowedActivity = this.isAllowedActivity(
         date,
@@ -525,7 +556,7 @@ export const useStore = defineStore('main', {
       );
 
       if (!isAllowedActivity) {
-        if (foundTimeMatches.length == 0) {
+        if (foundMatches[time].length == 0) {
           return result;
         } else
           result.reasons.push(
@@ -541,28 +572,28 @@ export const useStore = defineStore('main', {
 
       if (activity.perSession) {
         const minMax = getMinMax(activity.perSession);
-        if (foundTimeMatches.length < minMax[0]) {
+        if (foundTimeMatches < minMax[0]) {
           result.reasons.push(
-            `PerSession: expected >= ${minMax[0]}, found ${foundTimeMatches.length}`
+            `PerSession: expected >= ${minMax[0]}, found ${foundTimeMatches}`
           );
         }
-        if (foundTimeMatches.length > minMax[1]) {
+        if (foundTimeMatches > minMax[1]) {
           result.reasons.push(
-            `PerDay: expected <= ${minMax[1]}, found ${foundTimeMatches.length}`
+            `PerDay: expected <= ${minMax[1]}, found ${foundTimeMatches}`
           );
         }
       }
 
       if (activity.perDay) {
         const minMax = getMinMax(activity.perDay);
-        if (foundDateMatches.length < minMax[0]) {
+        if (foundDateMatches < minMax[0]) {
           result.reasons.push(
-            `PerDay: expected >= ${minMax[0]}, found ${foundDateMatches.length}`
+            `PerDay: expected >= ${minMax[0]}, found ${foundDateMatches}`
           );
         }
-        if (foundDateMatches.length > minMax[1]) {
+        if (foundDateMatches > minMax[1]) {
           result.reasons.push(
-            `PerDay: expected <= ${minMax[1]}, found ${foundDateMatches.length}`
+            `PerDay: expected <= ${minMax[1]}, found ${foundDateMatches}`
           );
         }
       }
@@ -575,14 +606,16 @@ export const useStore = defineStore('main', {
     // SmoView utilities
 
     getAssignedActivities(date: Date, time: Time, smoName: string) {
-      // return new Array<RosterEntry>();
-      return this.roster.filter(
-        (entry) =>
-          isSameDay(entry.date, date) &&
-          entry.time == time &&
-          entry.smo == smoName &&
-          entry.activity != 'Call'
+      return this.getRosterAtTime(date, time).filter(
+        (entry) => entry.smo == smoName && entry.activity != 'Call'
       );
+      // return this.roster.filter(
+      //   (entry) =>
+      //     isSameDay(entry.date, date) &&
+      //     entry.time == time &&
+      //     entry.smo == smoName &&
+      //     entry.activity != 'Call'
+      // );
     },
     getAllowedActivities(smoName: string) {
       return this.getSMO(smoName).activities;
